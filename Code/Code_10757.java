b60517c59d46768ef3497d9966d6bce2805281a2
choitaehoon <xognsdl1230@naver.com>
2018-07-11 17:19:37 +0900

    practice

---
+++ Code/Code_10757.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Code_10757
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextBigInteger().add(input.nextBigInteger()));
+    }
+}
---
+++ Code/Code_10827.java
@@ -0,0 +1,12 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Code_10827
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextBigDecimal().pow(input.nextInt()).toPlainString());
+
+    }
+}
