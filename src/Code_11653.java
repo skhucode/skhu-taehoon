bf74c8bcaa5340b4a6a879189998c47f87d1b6b5
choitaehoon <xognsdl1230@naver.com>
2018-08-17 11:26:31 +0900

    practice

--- src/Code_11653.java
+++ src/Code_11653.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
 
-public class Code_11576
+public class Code_11653
 {
     public static void main(String[] args) throws IOException
     {
